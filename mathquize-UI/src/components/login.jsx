0a43978aee04e94a7af00b4c48d0e9a2c03c62df
Sheshankumar Sutariya <sanketsutariya1482@gmail.com>
2025-07-13 22:37:03 +0530

    Update login.jsx

--- mathquize-UI/src/components/login.jsx
+++ mathquize-UI/src/components/login.jsx
@@ -3,7 +3,7 @@ import { auth, db } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 const Login = ({ setUser }) => {
   const [email, setEmail] = useState("");
